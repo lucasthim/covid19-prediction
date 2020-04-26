416b664c705796c8a15665176a0dbe6cbc865934
Lucas Thimoteo <lucasthim@yahoo.com>
2020-04-26 19:16:25 -0300

    Improved several methods in utils. Also got some solid results

--- utils/feature_importance_utils.py
+++ utils/feature_importance_utils.py
@@ -5,60 +5,39 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Patch
 
 
-def create_feature_importance_by_feature_weight(features,model):
-    
-    '''
-        Create a Dataframe based on the Weights given to each feature by regression models, with the following columns:
-            Weights: Weights of the models
-            abs_Weights: Absolute value of the weights
-            feature_importance: Normalized weights (all values of this column add up to one)
-            abs_feature_importance: Absolute value of the feature importance
-        
-        Parameters:
-            features: vector with the feature names
-            model: trained model object. Must have the "coef_" parameter.
-    '''
-
-    df_weights = pd.DataFrame(index = features,columns = ['Weights'])
-    df_weights['Weights'] = model.coef_.ravel()
-    df_weights['abs_Weights'] = np.abs(df_weights['Weights'])
-    total_weights = df_weights['abs_Weights'].values.ravel().sum()
-    df_weights['feature_importance'] = df_weights['Weights'].values / total_weights
-    df_weights['abs_feature_importance'] = df_weights['abs_Weights'].values / total_weights
-    df_weights.sort_values(by=['abs_feature_importance'],ascending = False,inplace = True)
-    return df_weights
-
-def show_feature_importance_by_feature_weight(df_weights,model_title, color = None, absolute_values = False, feature_importance = False,figsize=(8,8)):
+def show_feature_weights(features, coefficients,model_title, color = None, absolute_values = False, normalized = False,figsize=(8,8)):
 
     '''
-        Show a feature importance bar plot by model feature weights.
+        Show a feature importance bar plot by feature weights.
+
 
         Parameters:
 
-        df_weights: Dataframe created by the function create_feature_importance_by_feature_weight(). 
-        Contains the weights of the features and also their feature importances (values that add up to one)
+        features: vector with the feature names
 
-        model_title: Title of the model to plot
+        coefficients: 1-D array values of weights given to features.
 
-        color: Tuple to give different colors to positive and negative weights. Example color = ('red','green')
+        model_title: Name of the model
+
+        color: Tuple to give different colors to positive and negative weights. Example: color = ('red','green')
 
         absolute_values: Flag to analyse just absolute values of weights
 
-        feature_importance: Flag to select feature importance instead of weights 
+        normalized: Flag to normalize the feature weights 
 
     '''
-
+    df_weights = create_normalized_by_feature_weight(features,coefficients);
     column = 'Weights'
     if absolute_values:
         column = 'abs_Weights'
-    if feature_importance:
-        column = column.replace('Weights','feature_importance')
+    if normalized:
+        column = column.replace('Weights','normalized')
 
     df_weights = df_weights.sort_values(by=[column],ascending = True,inplace = False)
     fig,ax = plt.subplots(1,figsize=figsize)
     colors = (0.2,0.4,0.8)
     if color is not None:
-        color_mask = df_weights['feature_importance'] > 0
+        color_mask = df_weights['normalized'] > 0
         colors = [color[0] if c else color[1] for c in color_mask]
         legend_elements = [
             Patch(facecolor = color[1], edgecolor='k', label='Negative Contribution'),
@@ -66,16 +45,44 @@ def show_feature_importance_by_feature_weight(df_weights,model_title, color = No
     
     ax.tick_params(axis = 'both',labelsize = 'large')
     df_weights[column].plot(kind = 'barh', grid = True, color = colors,edgecolor='k', alpha = 0.6,ax = ax)
-    fig.suptitle(f'Feature Importance by Feature Weight - {model_title}',x = 0.3,fontsize = 20)
+    fig.suptitle(f'Feature Weights - {model_title}',x = 0.3,fontsize = 20)
 
     if color:
         ax.legend(handles=legend_elements, bbox_to_anchor=(1, -0.05), borderaxespad=0.,fancybox=True, shadow=True,ncol = 2)
 
-    if feature_importance:
+    if normalized:
         plt.figtext(0.91, 0.03, '*All absolute values add up to one', horizontalalignment='right')
     if absolute_values:
         plt.figtext(0.91, 0.01, '*All values are absolute', horizontalalignment='right')
 
     plt.subplots_adjust(top=0.93, bottom=0, left=0.10, right=0.95, hspace=0.40, wspace=0.35)
     plt.show()
+
+
+def create_normalized_by_feature_weight(features,coefficients):
+    
+    '''
+        Create a Dataframe based on the Weights given to each feature by regression models, with the following columns:
+            Weights: Weights of each feature
+            abs_Weights: Absolute value of the weights
+            normalized: Normalized weights (all values of this column add up to one)
+            abs_normalized: Absolute value of the feature importance
+        
+
+        Parameters:
+
+            features: 1-D array with the feature names
+
+            coefficients: 1-D array values of weights given to features.
+    '''
+
+    df_weights = pd.DataFrame(index = features,columns = ['Weights'])
+    df_weights['Weights'] = coefficients
+    df_weights['abs_Weights'] = np.abs(df_weights['Weights'])
+    total_weights = df_weights['abs_Weights'].values.ravel().sum()
+    df_weights['normalized'] = df_weights['Weights'].values / total_weights
+    df_weights['abs_normalized'] = df_weights['abs_Weights'].values / total_weights
+    df_weights.sort_values(by=['abs_normalized'],ascending = False,inplace = True)
+    return df_weights
+
     
--- utils/model_utils.py
+++ utils/model_utils.py
@@ -5,45 +5,91 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import confusion_matrix,recall_score, classification_report, auc, roc_curve,roc_auc_score
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
+from sklearn.feature_selection import RFECV
+from sklearn.metrics import confusion_matrix,recall_score, classification_report, auc, roc_curve,roc_auc_score, fbeta_score
 
 plt.style.use('seaborn')
 
-def predict(model,X,y):
-    df_result = pd.DataFrame(columns = ['TrueClass','Predicted'])
-    df_result.Predicted = model.predict(X.values)
-    df_result.TrueClass = y.values.ravel()
-    return df_result
-
-def find_best_classification_model_with_cross_validation(model,parameters,X_train,y_train,k_folds = 10,metric = 'f1',verbose = 0):
+def model_fit_with_grid_search_cv(model,parameters,X,y,folds = 5, score = 'accuracy',verbose = 0):
     start = time.time()
     grid_search = GridSearchCV(
         estimator = model,
         param_grid = parameters,
-        cv = k_folds,
-        scoring = metric, 
+        cv = StratifiedKFold(folds),
+        scoring = score, 
         verbose = verbose,
         n_jobs = -1)
-    grid_search.fit(X_train,y_train)
+    grid_search.fit(X,y)
 
     if verbose > 0:
         print("--- Ellapsed time: %s seconds ---" % (time.time() - start))
         print('Best params: ',grid_search.best_params_)
-        print('Best score (%s)' % metric,grid_search.best_score_)
+        print('Best score (%s)' % score,grid_search.best_score_)
     return grid_search.best_estimator_,grid_search.best_params_, grid_search.best_score_
 
-def evalute_model_performance(model,model_name,X,y,df_result):
+def model_fit_with_rfe_cv(model,X,y,folds = 5, score = 'f1',verbose = 0):
+    
+    rfecv = RFECV(
+        estimator=model, 
+        step=1, 
+        cv=StratifiedKFold(folds),
+        scoring=score,
+        n_jobs = -1)
+    rfecv.fit(X, y)
+    
+    selected_features = X.columns[rfecv.support_]
+    highest_score = rfecv.grid_scores_.max();
+    best_model = rfecv.estimator
+    
+    if verbose > 0:
+        print("Optimal number of features : %d" % rfecv.n_features_)
+        print('highest score: ', rfecv.grid_scores_.max())
+    if verbose > 1:
+        show_rfe_scores(rfecv);
+    return selected_features, highest_score, best_model, rfecv
+
+def show_rfe_scores(rfe):
+    plt.figure()
+    plt.title('RFE Scores')
+    plt.xlabel("Number of features selected")
+    plt.ylabel("Cross validation score (accuracy)")
+    plt.plot(range(1, len(rfe.grid_scores_) + 1), rfe.grid_scores_)
+    plt.show()
+
+
+def predict(model,X,y):
+    df_result = pd.DataFrame(columns = ['TrueClass','Predicted'])
+    df_result.Predicted = model.predict(X)
+    df_result.TrueClass = y.values.ravel()
+    return df_result
+    
+
+def evalute_model_performance(model,model_name,X,y):
+    df_result = predict(model,X,y)
+    class_report = classification_report(df_result.TrueClass, df_result.Predicted,output_dict = True)
+    accuracy = class_report['accuracy']
+    recall = class_report['macro avg']['recall']
+    precision = class_report['macro avg']['precision']
+    f1 = class_report['macro avg']['f1-score']
+    f2 = fbeta_score(df_result.TrueClass, df_result.Predicted,beta = 2, average='macro')
+    print('')
+    print('Performance Report: ')
+    print('Accuracy: %1.3f' % accuracy)
+    print('Recall: %1.3f' % recall)
+    print('Precision: %1.3f' % precision)
+    print('F1: %1.3f' % f1)
+    print('F2: %1.3f' % f2)
+    print('')
+
     plot_confusion_matrix(df_result,model_name)
-    acc_report = classification_report(df_result.TrueClass, df_result.Predicted,target_names =['Negative', 'Positive'])
-    print(acc_report)
+
     try:
         plot_ROC(model, model_name, X, y)
     except:
         print('Could not print ROC AUC curve.')
 
-
-def plot_confusion_matrix(df,title,labels = ['Negative', 'Positive'],dataset_type = 'Validation'):
+def plot_confusion_matrix(df,title,labels = ['Negative', 'Positive'],dataset_type = 'Test'):
     conf_matrix = confusion_matrix(df.TrueClass, df.Predicted)
     plt.figure(figsize=(8, 8))
     sns.heatmap(conf_matrix, xticklabels=labels, yticklabels=labels, annot=True, fmt="d");
@@ -52,7 +98,6 @@ def plot_confusion_matrix(df,title,labels = ['Negative', 'Positive'],dataset_typ
     plt.ylabel('True class')
     plt.show()
     return conf_matrix.ravel()
-    
 
 def plot_ROC(model,model_name,X_test,y_test):
     
